7e63f45d408a0840151e727faac8d10045705834
Aurongojeb Lishad <aurongojeblishad02@gmail.com>
2024-11-22 06:48:55 +0600

    database

--- borrow_book/index.php
+++ borrow_book/index.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * index.php
- * Handles the routing of requests to the borrow controller.
+ 
  */
 
 require_once 'config/database.php';
 require_once 'controllers/borrow_controller.php';
 
 // Create instance of borrow controller
-//$controller = new borrow_controller();
+$controller = new borrow_controller();
 
 // Handle POST request for borrowing a book
 if ($_SERVER['REQUEST_METHOD'] === 'POST') 
