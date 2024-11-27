a5f9b23034dad031eb40aca490e3ae41a456c14f
Aurongojeb Lishad <aurongojeblishad02@gmail.com>
2024-11-28 00:14:25 +0600

    changes in css and html file and add background image

--- borrow_book/views/borrow_form.php
+++ borrow_book/views/borrow_form.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Borrow Book</title>
+    <title>Wanna Borrow Book</title>
     <link rel="stylesheet" href="public/css/styles.css">
 </head>
 
@@ -12,7 +12,7 @@
     <div class="form-container">
         <h2>Borrow a Book</h2>
         <form action="index.php" method="POST">
-            <label for="email">User Email:</label>
+            <label for="email">Your Registered Email:</label>
             <input type="email" id="email" name="email" required>
 
             <label for="book_id">Book ID:</label>
